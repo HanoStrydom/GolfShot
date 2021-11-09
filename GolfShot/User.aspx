bb0b29efbb8343d68b2ca7783ca338d5a64b0ffc
Hano Strydom <hanostrydom8@gmail.com>
2021-11-09 20:39:41 +0200

    Added a new page for the viewer to view his own photos. TODO: 1) Use cookies to display user photos 2) Add delete button

---
+++ GolfShot/User.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="User.aspx.cs" Inherits="GolfShot.User" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 417px;
+        }
+        .auto-style2 {
+            text-align: center;
+        }
+        .auto-style3 {
+            width: 417px;
+            text-align: center;
+        }
+        .auto-style4 {
+            width: 417px;
+            height: 23px;
+        }
+        .auto-style5 {
+            height: 23px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:DataList ID="DataList1" runat="server">
+            <ItemTemplate>
+                <table> 
+                    <tr> 
+                        <td> <img src="<%#Eval("ImagePath") %>" height ="200", width = "200" /></td>
+                    </tr>
+                </table>
+            </ItemTemplate>
+        </asp:DataList>
+
+           
+
+            
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ GolfShot/User.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GolfShot
+{
+    public partial class User : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=cmpg323-project2.database.windows.net;Initial Catalog=Project2;User ID=Hano;Password=3695748Hl;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from ImageToDB WHERE Uploader = 'Hano'";
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                DataList1.DataSource = dt;
+                DataList1.DataBind();
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script> alert('Error!')</script>");
+            }
+        }
+    }
+}
